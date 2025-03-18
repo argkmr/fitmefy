966cede48d9bce94d4b2f8991b51c799e738b896
anuragkumar <argkmr@gmail.com>
2025-03-18 13:26:00 +0530

    backend code first commit

---
+++ fitmefy-backend/src/main/java/com/fitmefy_backend/FitmefyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fitmefy_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitmefyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitmefyBackendApplication.class, args);
+	}
+
+}
